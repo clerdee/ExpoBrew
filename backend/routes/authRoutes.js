5339e8829921f34c73d95ba35688d63328412f81
clerdee <clerdeecruz@gmail.com>
2026-03-12 01:26:32 +0800

    auth route with profile image

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController'); 
+const upload = require('../config/cloudinary');
 
-router.post('/register', registerUser);
+router.post('/register', upload.single('profileImage'), registerUser);
 router.post('/login', loginUser);
 
 module.exports = router;
