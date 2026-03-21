dd0625092861cf6e593c673e01d8f6094fd13721
clerdee <clerdeecruz@gmail.com>
2026-03-21 22:58:38 +0800

    fav last fix for 404

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,6 @@ const deleteUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) return res.status(404).json({ message: "User not found." });
-    
     await user.deleteOne();
     res.status(200).json({ message: "User deleted successfully!" });
   } catch (e) { res.status(500).json({ message: "Server Error: Could not delete user." }); }
