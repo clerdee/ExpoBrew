88d8477c7684e35e27d7c9618a4b5447d4370f35
clerdee <clerdeecruz@gmail.com>
2026-03-23 02:39:42 +0800

    auth logins?

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, verifyOtp } = require('../controllers/authController'); 
+const { registerUser, loginUser, verifyOtp, googleLogin, facebookLogin } = require('../controllers/authController'); 
 const upload = require('../config/cloudinary');
 
 router.post('/register', registerUser);
