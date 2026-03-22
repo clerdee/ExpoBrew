9b12f03a737329ed10dd8421dfad8bbe6e9a4fca
clerdee <clerdeecruz@gmail.com>
2026-03-22 16:43:38 +0800

    review emai ok

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { getDashboardStats, createPromo, getPromos, getUsers, deactivateUser, getAllReviews, deleteReview, updateOrderStatus } = require('../controllers/adminController');
 const { protect, admin } = require('../middleware/authMiddleware');
+const { getAllUsers } = require('../controllers/userController');
 
 router.get('/dashboard', protect, admin, getDashboardStats);
 router.put('/orders/:id/status', protect, admin, updateOrderStatus);
 router.route('/promos').get(protect, admin, getPromos).post(protect, admin, createPromo);
-router.get('/users', protect, admin, getUsers);
+router.get('/users', protect, admin, getAllUsers);
 router.put('/users/:id/deactivate', protect, admin, deactivateUser);
 router.route('/reviews').get(protect, admin, getAllReviews);
 router.route('/reviews/:id').delete(protect, admin, deleteReview);
