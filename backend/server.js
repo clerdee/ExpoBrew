93833cac6562611e548f8dd20d9324bc1798d0c5
clerdee <clerdeecruz@gmail.com>
2026-03-22 22:02:41 +0800

    new server.js

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,16 @@ const reviewRoutes = require('./routes/reviewRoutes');
 
 const app = express();
 
-app.use(express.json()); 
-
-app.use(cors());
-
 app.use(cors());
 app.use(express.json()); 
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.log("❌ MongoDB connection error:", err));
+
+app.get('/', (req, res) => {
+  res.send('ExpoBrew API is running!');
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
@@ -33,5 +33,5 @@ app.use('/api/reviews', reviewRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
